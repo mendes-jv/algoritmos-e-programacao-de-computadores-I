783f0ee72fe03e0be75d7613b4b74401ac472724
João Victor Mendes da Silva <mendes.silva.jv@gmail.com>
2022-04-20 23:26:01 -0300

    Commit Incial

---
+++ main.py
@@ -0,0 +1,72 @@
+# Exercícios de matéria de Algoritmos e Programação de Computadores I
+
+# exercício 2.1
+
+a1 = 1 + 2 + 3 + 4 + 5
+print(a1)
+
+b1 = (23 + 19 + 32) / 3
+print(b1)
+
+c1 = 403 // 73
+print(c1)
+
+d1 = 403 % 73
+print(d1)
+
+e1 = 2 ** 10
+print(e1)
+
+f1 = abs(54 - 57)
+print(f1)
+
+g1 = min(34.99, 29.95, 31.5)
+print(g1)
+
+# exercício 2.2
+
+a2 = (2 + 2) < 4
+print(a2)
+
+b2 = (7 // 3) == (1 + 1)
+print(b2)
+
+c2 = (3 ** 2 + 4 ** 2) == 25
+print(c2)
+
+d2 = (2 + 4 + 6) > 12
+print(d2)
+
+e2 = (1387 % 19) == 0
+print(e2)
+
+f2 = (31 % 2) == 0
+print(f2)
+
+g2 = min(34.99, 29.95, 31.5) < 30.0
+print(g2)
+
+# exercício 2.3
+
+a = 3
+b = 4
+c = (a * a) + (b * b)
+print(c)
+
+# exercício 2.4
+
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+
+print(s1 + " " + s2 + " " + s1)
+print(((s1 + " ") * 9) + s1)
+print(s1 + " " + ((s2 + " ") * 2) + ((s3 + " ") * 2) + s3)
+print(((s1 + " " + s2 + " ") * 7) + s1 + " " + s2)
+print(((((s2 * 2) + s3) + " ") * 4) + ((s2 * 2) + s3))
+
+# exercício 2.5
+
+s = '0123456789'
+
+print(s[0], s[1], s[6], s[8], s[9], sep="\n")
